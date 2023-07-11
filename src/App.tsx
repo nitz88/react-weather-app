7463541ea305b8eee43ced3506d997cc8044b61f
Nitish <nitish.surelia@netcracker.com>
2023-07-11 18:24:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header/Header';
+import Weather from './Weather/Weather';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Weather />
+    </>
+  )
+}
+
+export default App
---
+++ src/Header/Header.tsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import './Header.css'
+
+function Header() {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const changeDarkModeToggle = (checkboxValue: boolean) => {
+        setDarkMode(checkboxValue);
+        changeBodyClass(checkboxValue);
+    }
+
+    const changeCheckBoxValue = (toggle: boolean) => {
+        changeDarkModeToggle(toggle);
+    }
+    
+    const changeBodyClass = (checkboxValue: boolean) => {
+        if (checkboxValue) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }
+
+    return (
+        <>
+        <header className='my-8'>
+            <div className="grid justify-items-end">
+                <div className="flex">
+                    <span className="inline-block pl-[0.15rem] hover:cursor-pointer" onClick={() => changeCheckBoxValue(false)}>
+                        <svg className="h-6 w-6 text-gray-500 dark:text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                        </svg>
+                    </span>
+                    <input
+                        className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+                        type="checkbox"
+                        role="switch"
+                        id="flexSwitchCheckDefault"
+                        checked={darkMode}
+                        onChange={(event) => {changeDarkModeToggle(event.target.checked)}} />
+                    <span className="inline-block pl-[0.15rem] hover:cursor-pointer" onClick={() => changeCheckBoxValue(true)}>
+                        <svg className="h-6 w-6 text-gray-400 dark:text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </header>
+        </>
+    );
+}
+
+export default Header;
